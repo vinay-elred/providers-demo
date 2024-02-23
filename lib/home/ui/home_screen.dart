fe96557d3538de5dad661d598ba9b1632c7b3e82
Vinay_Sharma <vinay@elred.io>
2024-02-23 15:09:14 +0530

    context.read, watch, select

--- lib/home/ui/home_screen.dart
+++ lib/home/ui/home_screen.dart
@@ -23,63 +23,75 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<HomeScreenViewModel>.value(
       value: homeVM,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Home"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Consumer<HomeScreenViewModel>(
-                builder: (context, homeVM, _) {
-                  if (homeVM.isFetching) {
-                    return const CircularProgressIndicator();
-                  }
-
-                  if (homeVM.data.isEmpty) return const Text("Data error");
-
-                  return SizedBox(
-                    width: 200,
-                    child: ListView.separated(
-                      shrinkWrap: true,
-                      itemCount: homeVM.data.length,
-                      itemBuilder: (context, index) {
-                        final item = homeVM.data[index];
-                        return Text(
-                          item,
-                          textAlign: TextAlign.center,
-                        );
-                      },
-                      separatorBuilder: (context, index) {
-                        return const Divider();
-                      },
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 50),
-              ElevatedButton(
-                onPressed: () {
-                  homeVM.fetchData();
-                },
-                child: const Text("Fetch Again"),
-              ),
-            ],
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Home"),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (_) => ChangeNotifierProvider.value(
-                value: homeVM,
-                child: const CartScreen(),
-              ),
-            ));
-          },
-          child: const Icon(Icons.navigate_next_outlined),
-        ),
-      ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<HomeScreenViewModel>(
+                  builder: (context, homeVM, _) {
+                    if (homeVM.isFetching) {
+                      return const CircularProgressIndicator();
+                    }
+
+                    if (homeVM.data.isEmpty) return const Text("Data error");
+
+                    return SizedBox(
+                      width: 200,
+                      child: ListView.separated(
+                        shrinkWrap: true,
+                        itemCount: homeVM.data.length,
+                        itemBuilder: (context, index) {
+                          final item = homeVM.data[index];
+                          return Text(
+                            item,
+                            textAlign: TextAlign.center,
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return const Divider();
+                        },
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 50),
+                Text(
+                  "List len : ${context.watch<HomeScreenViewModel>().data.length}",
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "List Contain Apple : ${context.select<HomeScreenViewModel, bool>(
+                    (p) => p.data.contains("Apple"),
+                  )}",
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<HomeScreenViewModel>().fetchData();
+                  },
+                  child: const Text("Fetch Again"),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => ChangeNotifierProvider.value(
+                  value: homeVM,
+                  child: const CartScreen(),
+                ),
+              ));
+            },
+            child: const Icon(Icons.navigate_next_outlined),
+          ),
+        );
+      },
     );
   }
 }
